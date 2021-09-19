e91883b3c50692ec464577789dde5e6a61bb6ceb
Arvindan <arvindan92@gmail.com>
2021-09-19 18:15:10 +0530

    basic lerna setup with core and react stencil wrapper

--- packages/fw-crayons-react/dist/esm/components.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import type { JSX } from 'fw-crayons/dist/components';
-export declare const FwButton: import("react").ForwardRefExoticComponent<JSX.FwButton & Omit<import("react").HTMLAttributes<HTMLFwButtonElement>, "style"> & import("./react-component-lib/interfaces").StyleReactProps & import("react").RefAttributes<HTMLFwButtonElement>>;
-export declare const MyComponent: import("react").ForwardRefExoticComponent<JSX.MyComponent & Omit<import("react").HTMLAttributes<HTMLMyComponentElement>, "style"> & import("./react-component-lib/interfaces").StyleReactProps & import("react").RefAttributes<HTMLMyComponentElement>>;
